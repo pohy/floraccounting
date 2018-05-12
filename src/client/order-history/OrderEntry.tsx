be1b143f0df7221af0e0515372778f5061bb6488
David Pohan <0ronon0@gmail.com>
2018-05-12 13:06:28 +0200

    Add badge to order listing

--- src/client/order-history/OrderEntry.tsx
+++ src/client/order-history/OrderEntry.tsx
@@ -2,28 +2,45 @@ import React from 'react';
 import { SFC } from 'react';
 import { Transaction } from '../../common/model/Transaction';
 import './OrderEntry.css';
+import { Img } from '../common/Img';
 
 export interface IOrderEntryProps {
     transaction: Transaction;
 }
 
 export const OrderEntry: SFC<IOrderEntryProps> = ({
-    transaction: { items, price, currency, created },
-}) => (
-    <div className="OrderEntry flex column padding">
-        <div className="flex">
-            <span className="items grow">
-                {items.map(({ name }) => name).join(', ')}
-            </span>
-            <span className="price">
-                {price}&nbsp;{currency}
+    transaction: { items, price, currency, created, user },
+}) => {
+    const profilePicture = user && user.profilePictureURL;
+    return (
+        <div className="OrderEntry flex padding">
+            {profilePicture && (
+                <span className="picture">
+                    <Img
+                        className="badge"
+                        src={profilePicture}
+                        alt={user.name}
+                    />
+                </span>
+            )}
+            <span className="grow">
+                <div className="flex">
+                    <span className="items grow">
+                        {items.map(({ name }) => name).join(', ')}
+                    </span>
+                    <span className="price">
+                        {price}&nbsp;{currency}
+                    </span>
+                </div>
+                <div className="info flex">
+                    <span className="time grow">
+                        {created.toLocaleString('en-US')}
+                    </span>
+                    <a href="#" className="more">
+                        More...
+                    </a>
+                </div>
             </span>
         </div>
-        <div className="info flex">
-            <span className="time grow">{created.toLocaleString('en-US')}</span>
-            <a href="#" className="more">
-                More...
-            </a>
-        </div>
-    </div>
-);
+    );
+};
