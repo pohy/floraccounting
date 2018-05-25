6db9c61a4e5c8552e972dfca7d8b8859dd3a1684
David Pohan <0ronon0@gmail.com>
2018-05-25 18:49:45 +0200

    Fix order history layout

--- src/client/order-history/OrderEntry.tsx
+++ src/client/order-history/OrderEntry.tsx
@@ -13,34 +13,36 @@ export const OrderEntry: SFC<IOrderEntryProps> = ({
 }) => {
     const profilePicture = user && user.profilePictureURL;
     return (
-        <div className="OrderEntry flex padding">
-            {profilePicture && (
-                <span className="picture">
-                    <Img
-                        className="badge"
-                        src={profilePicture}
-                        alt={user.name}
-                    />
+        <div className="OrderEntry padding">
+            <div className="flex">
+                {profilePicture && (
+                    <span className="picture">
+                        <Img
+                            className="badge"
+                            src={profilePicture}
+                            alt={user.name}
+                        />
+                    </span>
+                )}
+                <span className="grow">
+                    <div className="flex">
+                        <span className="items grow">
+                            {items.map(({ name }) => name).join(', ')}
+                        </span>
+                        <span className="price">
+                            {price}&nbsp;{currency}
+                        </span>
+                    </div>
+                    <div className="info flex">
+                        <span className="time grow">
+                            {created.toLocaleString('en-US')}
+                        </span>
+                        <a href="#" className="more">
+                            More...
+                        </a>
+                    </div>
                 </span>
-            )}
-            <span className="grow">
-                <div className="flex">
-                    <span className="items grow">
-                        {items.map(({ name }) => name).join(', ')}
-                    </span>
-                    <span className="price">
-                        {price}&nbsp;{currency}
-                    </span>
-                </div>
-                <div className="info flex">
-                    <span className="time grow">
-                        {created.toLocaleString('en-US')}
-                    </span>
-                    <a href="#" className="more">
-                        More...
-                    </a>
-                </div>
-            </span>
+            </div>
         </div>
     );
 };
